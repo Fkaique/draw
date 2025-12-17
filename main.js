d1c4683d2d9208c13342c172a50f0c06cb8049e0
Francisco Kaique <fkaiquesp@gmail.com>
2025-12-17 00:35:27 -0300

    replace: aceitando toques

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
 
 document.body.appendChild(canvas)
 
 let pressed = false
 
-canvas.addEventListener('mousedown', ()=>{
+canvas.addEventListener('pointerdown', ()=>{
     pressed = true    
 })
 
-canvas.addEventListener('mouseup', ()=>{
+function resize() {
+  const dpr = window.devicePixelRatio || 1
+
+  canvas.width  = window.innerWidth * dpr
+  canvas.height = window.innerHeight * dpr
+
+  canvas.style.width  = window.innerWidth + 'px'
+  canvas.style.height = window.innerHeight + 'px'
+
+  ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
+}
+resize()
+
+canvas.addEventListener('pointerup', ()=>{
     pressed = false
 })
-document.addEventListener('mouseup', ()=>{
+document.addEventListener('pointerup', ()=>{
     pressed = false
 })
 
@@ -27,18 +38,18 @@ canvas.addEventListener('click', (e)=>{
     const my = e.clientY
     ctx.beginPath()
     ctx.fillStyle = 'black'
-    ctx.arc(mx-5, my-5, size, 0, Math.PI * 2)
+    ctx.arc(mx+5, my+5, size, 0, Math.PI * 2)
     ctx.fill()
 })
 
-canvas.addEventListener('mousemove', (e)=>{
+canvas.addEventListener('pointermove', (e)=>{
     if (!pressed) {return};
     const rect = canvas.getBoundingClientRect()
     const mx = e.clientX
     const my = e.clientY
     ctx.beginPath()
     ctx.fillStyle = 'black'
-    ctx.arc(mx-5, my-5, size, 0, Math.PI * 2)
+    ctx.arc(mx+5, my+5, size, 0, Math.PI * 2)
     ctx.fill()
 
 })

46051886200a1477688c030558f79dcdee50dc92
Francisco Kaique <fkaiquesp@gmail.com>
2025-12-17 02:09:34 -0300

    refactor: resize

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
 const canvas = document.createElement('canvas')
+const container = document.getElementById('container')
 const ctx = canvas.getContext('2d')
 
-
-document.body.appendChild(canvas)
-
-let pressed = false
-
-canvas.addEventListener('pointerdown', ()=>{
-    pressed = true    
-})
-
 function resize() {
-  const dpr = window.devicePixelRatio || 1
-
-  canvas.width  = window.innerWidth * dpr
-  canvas.height = window.innerHeight * dpr
-
-  canvas.style.width  = window.innerWidth + 'px'
-  canvas.style.height = window.innerHeight + 'px'
-
-  ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
+    console.log('neh')
+    const rect = container.getBoundingClientRect()
+    const saved = ctx.getImageData(0,0,canvas.width,canvas.height)
+    canvas.width  = rect.width
+    canvas.height = rect.height
+    ctx.putImageData(saved,0,0)
 }
 resize()
 
-canvas.addEventListener('pointerup', ()=>{
-    pressed = false
-})
-document.addEventListener('pointerup', ()=>{
-    pressed = false
-})
+const resizeObserver = new ResizeObserver(resize)
+resizeObserver.observe(container)
 
+container.appendChild(canvas)
+
+let pressed = false
 let size = 4
 
-canvas.addEventListener('click', (e)=>{
+canvas.addEventListener('pointerdown', (e)=>{
+    if (e.button!=0) return;
+    ctx.moveTo(e.clientX,e.clientY)
+    pressed = true  
+})
+
+
+document.addEventListener('pointerup', (e)=>{
+    if (e.button!=0) return;
+    pressed = false
     const rect = canvas.getBoundingClientRect()
     const mx = e.clientX
     const my = e.clientY
+    ctx.lineWidth = size
+    ctx.lineCap = "round"
+    ctx.lineJoin = "round"
+    ctx.lineTo(e.clientX,e.clientY)
+    ctx.stroke()
     ctx.beginPath()
-    ctx.fillStyle = 'black'
-    ctx.arc(mx+5, my+5, size, 0, Math.PI * 2)
-    ctx.fill()
+    
+    // ctx.beginPath()
+    // ctx.fillStyle = 'black'
+    // ctx.arc(mx+5, my+5, size, 0, Math.PI * 2)
+    // ctx.fill()
+    // console.log("neh")
 })
 
 canvas.addEventListener('pointermove', (e)=>{
@@ -47,11 +52,12 @@ canvas.addEventListener('pointermove', (e)=>{
     const rect = canvas.getBoundingClientRect()
     const mx = e.clientX
     const my = e.clientY
-    ctx.beginPath()
-    ctx.fillStyle = 'black'
-    ctx.arc(mx+5, my+5, size, 0, Math.PI * 2)
-    ctx.fill()
-
+    ctx.lineTo(e.clientX,e.clientY)
+    ctx.stroke()
+    // ctx.beginPath()
+    // ctx.fillStyle = 'black'
+    // ctx.arc(mx+5, my+5, size, 0, Math.PI * 2)
+    // ctx.fill()
 })
 
 document.addEventListener('keydown',(e)=>{
